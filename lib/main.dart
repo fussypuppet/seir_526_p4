858e4147e3935f53b31d2fa5fcf20353dbf9b01b
Eliott Davidson <ulthos@gmail.com>
2020-08-16 15:16:05 -0700

    Added code and API keys and packages necessary to display a google map, but have not yet actually linked to it in the app itself.  App boots without errors though

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 void main() => runApp(MyApp());
 
@@ -12,6 +13,38 @@ class MyApp extends StatelessWidget {
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
       home: MyHomePage(title: 'Plenty of Starfish'),
+    );
+  }
+}
+
+class MyPlanPage extends StatefulWidget {
+  @override
+  _MyPlanPageState createState() => _MyPlanPageState();
+}
+
+class _MyPlanPageState extends State<MyPlanPage> {
+  GoogleMapController mapController;
+  final LatLng _center = const LatLng(45.521563, -122.677433);
+  void _onMapCreated(GoogleMapController controller) {
+    mapController = controller;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("New Plan!"),
+          backgroundColor: Colors.green[700],
+        ),
+        body: GoogleMap(
+          onMapCreated: _onMapCreated,
+          initialCameraPosition: CameraPosition(
+            target: _center,
+            zoom: 11.0,
+          ),
+        ),
+      ),
     );
   }
 }
