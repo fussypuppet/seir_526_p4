a3b3e0b0ccef120c96d9a9509862c87a69dfd616
Eliott Davidson <ulthos@gmail.com>
2020-08-17 12:10:48 -0700

    Both iOS and Android now use hidden API keys, though iOS map still does not display:

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
-import './keys.dart' show IOS_KEY, ANDROID_KEY;
 
 void main() => runApp(MyApp());
 
